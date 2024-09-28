0428c9696b25fa891fa68e8f9c359b5822e29f6f
Marcelo A. Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-28 20:06:56 -0300

    feat: add mocked data

---
+++ src/data/database.ts
@@ -0,0 +1,23 @@
+import {Task} from "../types/TaskType.tsx";
+
+export const tasks:Task[] =
+[
+    {
+        id: 1,
+        title: 'teste1',
+        description: 'descricao teste1',
+        state: 'completed'
+    },
+    {
+        id: 2,
+        title: 'teste2',
+        description: 'descricao teste2',
+        state: 'pending'
+    },
+    {
+        id: 3,
+        title: 'teste3',
+        description: 'descricao teste3',
+        state: 'in_progress'
+    },
+];
