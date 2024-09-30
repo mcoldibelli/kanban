fd2a291b92d9575af988be9c1ce3e946234702fd
Marcelo A. Coldibelli <marcelo.coldibelli@gmail.com>
2024-09-29 22:40:58 -0300

    refactor: moves types to own folder and refactor id to string

--- src/types/TaskType.tsx
+++ src/types/TaskType.tsx
@@ -1,8 +1,26 @@
+import { DropResult } from "react-beautiful-dnd";
+
 export type TaskStatus = 'pending' | 'in_progress' | 'completed';
 
 export type Task = {
-    id: number;
+    id: string;
     title: string;
     description: string;
     status: TaskStatus;
 };
+
+export type TaskContextType = {
+    tasks: Task[];
+    addTask: (task: Task) => void;
+    updateTask: (id: string, updatedTask: Partial<Task>) => void;
+    deleteTask: (id: string) => void;
+    onDragEnd: (result: DropResult) => void;
+};
+
+export type TaskProps = {
+    id: string,
+    index: number,
+    content: Task,
+    onEdit: (id: string) => void,
+    onDelete: (id: string) => void
+}
